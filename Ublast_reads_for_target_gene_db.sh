62aed1e420980b452e6c529de482e6724fb5fb35
weber1107 <53131670+weber1107@users.noreply.github.com>
2021-05-14 10:50:33 +0800

    revised

--- Ublast_reads_for_target_gene_db.sh
+++ Ublast_reads_for_target_gene_db.sh
@@ -7,7 +7,7 @@ if [[ "$1" == "" || "$1" == "-h" ]] ; then
    Usage: ./Ublast_reads_for_target_genes.sh target_gene_dir reads_dir max_jobs
 
    [target_genes_dir]: Path to target genes, all the genes should be cat into one fasta file (it dosen't matter Nucleic or Protein sequence).
-   Note: The fa file should be named the same as the folder's name.
+   Note: The fa file should be the SAME named to the folder's name.
 
    [reads_dir]: Folder path to the clean reads must be in FastQ format, and filenames must follow the format: <name>_<sis>.fastq, where <name> is the sample name, and <sis> is 1 or 2 indicating which sister read the file contains. Use only '1' as <sis> if you only want to use single reads to generate a fast calculation.
 
@@ -23,17 +23,17 @@ else
     let MAX=$3+0 ;
 fi ;
 
-target_genes=$(readlink -f $1) ;
-target_gene_name=$(basename $target_genes)
+target_genes_dir=$(readlink -f $1) ;
+target_gene_name=$(basename ${target_genes_dir})
 reads_dir=$(readlink -f $2)
 pac=$(dirname $(readlink -f $0)) ;
 cwd=$(pwd) ;
 
 ## generate index and the length of target_genes
-sed -i 's/ /_/g' $target_genes/${target_gene_name}.fa
+sed -i 's/ /_/g' $target_genes_dir/${target_gene_name}.fa
 ### length
-if [ ! -f "$target_genes/${target_gene_name}_length" ] ; then
-  sed 's/ /_/g' $target_genes/${target_gene_name}.fa | awk '/^>/&&NR>1{print "";}{ printf "%s",/^>/ ? $0" ":$0 }' | awk '{print $1"\t"length($2)}' | sed -e 's/^>//' | sort -k1,1 > $target_genes/${target_gene_name}_length ;
+if [ ! -f "$target_genes_dir/${target_gene_name}_length" ] ; then
+  sed 's/ /_/g' $target_genes_dir/${target_gene_name}.fa | awk '/^>/&&NR>1{print "";}{ printf "%s",/^>/ ? $0" ":$0 }' | awk '{print $1"\t"length($2)}' | sed -e 's/^>//' | sort -k1,1 > $target_genes_dir/${target_gene_name}_length ;
 else
   ### index for ublast (optional)
   ## PATH/TO/usearch -makeudb_ublast $target_genes -output $target_genes/${target_gene_name}.udb
@@ -50,7 +50,7 @@ else
       b=$(basename $i) ;
       b_n=$(basename $i .fastq)
       # Launch job
-      jids[$k]=$(sbatch --export="READ_DIR=$reads_dir,SAMPLE=$b,TARGET=$target_genes/${target_gene_name}.fa" -J "ublast-$b_n" -o "out-$b_n.log" $pac/run_ublast.slurm | grep .) ;
+      jids[$k]=$(sbatch --export="READ_DIR=${reads_dir},SAMPLE=$b,TARGET_DIR=${target_genes_dir},TARGET_NAME=${target_gene_name}" -J "ublast-$b_n" -o "out-$b_n.log" $pac/run_ublast.slurm | grep .) ;
       echo "$b: ${jids[$k]}$EXTRA_MSG" ;
     fi ;
     let k=$k+1 ;
